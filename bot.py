19388b98cd466d122f9182e3ff256c98b8845c28
Roberto Díaz Pérez <r.r.1994a@gmail.com>
2017-08-14 13:41:52 +0100

    Author now gives a descrition

--- bot.py
+++ bot.py
@@ -50,9 +50,17 @@ AUTHOR_CALLSIGN = "!getauthor"
 TEMPLATE_BOOK = """
 {number}. {title} by {author}. ({rating}/5 *) [Link]({link})
 """
+TEMPLATE_AUTHOR = """
+[{name}]({link})
+
+
+>{description}
+
+"""
 SIGNATURE = """
 ***
 ^^I'm ^^a ^^bot, ^^check ^^me ^^out ^^at: ^^https://github.com/RoberTnf/BookBot
+^^Created ^^by ^^/u/AgressiveYorkshire.
 """
 
 # goodreads params
@@ -76,7 +84,7 @@ def get_search_strings(comments, callsign):
 
     return search_strings
 
-def get_books_info(search_strings, n=1):
+def get_books_info(search_strings):
     # parses goodread for info about books
     books_info = []
     for search_string in search_strings:
@@ -85,21 +93,16 @@ def get_books_info(search_strings, n=1):
         if '<h3 class="searchSubNavContainer">No results.</h3>' not in request_text:
             soup = BeautifulSoup(request_text, "html.parser")
             books_content = []
-            for book_content in soup.findAll("tr"):
-                if book_content["itemtype"] == "http://schema.org/Book":
-                    books_content.append(book_content)
-
             book_info = []
             # Avoid (books_info[-1]["link"]).text trying to read more books than exist
-            n = min(n, len(books_content))
+            books_content = soup.findAll("tr", {"itemtype":"http://schema.org/Book"})
+            n = min(NUMBER_OF_BOOKS, len(books_content))
             for book_content in books_content[:n]:
                 book_info.append({})
                 book_info[-1]["title"] = book_content.find("span", {"itemprop" : "name"}).string
                 book_info[-1]["author"] = book_content.find("a", { "class" : "authorName" }).span.string
                 book_info[-1]["rating"] = book_content.find("span", {"class": "minirating"}).contents[-1][1:5]
                 book_info[-1]["link"] = "https://www.goodreads.com" + book_content.find("a")["href"] +"&ac=1"
-
-            if n == 1:
                 request_text = get(book_info[-1]["link"]).text
                 soup = BeautifulSoup(request_text, "html.parser")
                 soup = soup.find("div", {"id":"description"})
@@ -107,18 +110,89 @@ def get_books_info(search_strings, n=1):
                 if soup:
                     book_html = soup.findAll("span")[-1].text
                     sub('<[^<]+?>', '', book_html)
-                    book_info[0]["description"] = book_html
-
+                    book_info[-1]["description"] = book_html
             books_info.append(book_info)
         else:
             books_info.append(False)
 
     return(books_info)
 
-def get_reply_strings(books_info):
+def get_authors_info(search_strings):
+    # parses goodread for info about books
+    books_info = []
+    authors_info = []
+    for search_string in search_strings:
+        request_text = get(SEARCH_URL + search_string,
+                           headers={'User-Agent': 'BookBot-Agent'}).text
+        if '<h3 class="searchSubNavContainer">No results.</h3>' not in request_text:
+            books_content = []
+            book_info = []
+            soup = BeautifulSoup(request_text, "html.parser")
+            authors_info.append({})
+
+            # go to the first author page
+            authors_info[-1]["link"] = soup.find("a", {"class":"authorName"})["href"]
+            request_text = get(soup.find("a", {"class":"authorName"})["href"]).text
+            soup = BeautifulSoup(request_text, "html.parser")
+            books_content = soup.findAll("tr", {"itemtype":"http://schema.org/Book"})
+
+            authors_info[-1]["link"] = soup.find("a", {"class":"authorName"})["href"]
+            authors_info[-1]["author"] = soup.find("a", {"class":"authorName"}).span.text
+            description_id = "freeTextauthor"+authors_info[-1]["link"].split("/")[-1].split(".")[0]
+            description_html = soup.find("span", id=description_id).text
+            sub('<[^<]+?>', '', description_html)
+            authors_info[-1]["description"] = description_html
+
+            # Avoid (books_info[-1]["link"]).text trying to read more books than exist
+            n = min(NUMBER_OF_BOOKS_AUTHOR, len(books_content))
+            for book_content in books_content[:n]:
+                book_info.append({})
+                book_info[-1]["title"] = book_content.find("span", {"itemprop" : "name"}).string
+                book_info[-1]["author"] = authors_info[-1]["author"]
+                book_info[-1]["rating"] = book_content.find("span", {"class": "minirating"}).contents[-1][1:5]
+                book_info[-1]["link"] = "https://www.goodreads.com" + book_content.find("a")["href"] +"&ac=1"
+                request_text = get(book_info[-1]["link"]).text
+                soup = BeautifulSoup(request_text, "html.parser")
+
+            books_info.append(book_info)
+        else:
+            books_info.append(False)
+            authors_info.append(False)
+    return(books_info, authors_info)
+
+def get_reply_strings_book(books_info):
     reply_texts = []
     for book_info in books_info:
         reply_text = ""
+        if book_info:
+            for i, book in enumerate(book_info):
+                reply_text += TEMPLATE_BOOK.format(
+                    number=i+1,
+                    title=book["title"],
+                    author=book["author"],
+                    rating=book["rating"],
+                    link=book["link"]
+                )
+            if "description" in book_info[0].keys():
+                reply_text += "\n\n>{}\n\n".format(book_info[0]["description"])
+            else:
+                reply_text += "\n\n>No description Foud\n\n"
+        else:
+            reply_text += "No results found."
+        reply_text += SIGNATURE
+        reply_texts.append(reply_text)
+    return(reply_texts)
+
+def get_reply_strings_author(books_info, authors_info):
+    reply_texts = []
+    for author_info, book_info in zip(authors_info, books_info):
+        reply_text = ""
+        if author_info:
+            reply_text += TEMPLATE_AUTHOR.format(
+                name=author_info["author"],
+                link=author_info["link"],
+                description=author_info["description"]
+            )
         if book_info:
             for i, book in enumerate(book_info):
                 reply_text += TEMPLATE_BOOK.format(
@@ -163,14 +237,10 @@ def reply(comments, reply_strings, table):
     for comment, reply_string in zip(comments, reply_strings):
 
         comment.reply(reply_string)
-
+        if DEBUG:
+            print("Replied to: {}".format(comment.permalink()))
         # saves to DB
         subreddit = comment.permalink().split("/")[2]
-        print(SQL_ADD_COMMENT.format(
-            tablename=table,
-            id=comment.fullname,
-            subreddit=subreddit,
-            now=datetime.now()))
         CURSOR.execute(SQL_ADD_COMMENT.format(
             tablename=table,
             id=comment.fullname,
@@ -182,21 +252,21 @@ def reply(comments, reply_strings, table):
 def main():
 
     # books
-    table="books"
+    """table="books"
     CURSOR.execute(SQL_CREATE_TABLE.format(tablename=table))
     comments = get_comments(BOOK_CALLSIGN, table)
     search_strings = get_search_strings(comments, BOOK_CALLSIGN)
     books_info = get_books_info(search_strings)
-    reply_strings = get_reply_strings(books_info)
-    reply(comments, reply_strings, table)
+    reply_strings = get_reply_strings_book(books_info)
+    reply(comments, reply_strings, table)"""
 
     #authors
-    table="authors"
+    table = "authors"
     CURSOR.execute(SQL_CREATE_TABLE.format(tablename=table))
     comments = get_comments(AUTHOR_CALLSIGN, table)
     search_strings = get_search_strings(comments, AUTHOR_CALLSIGN)
-    books_info = get_books_info(search_strings, n=5)
-    reply_strings = get_reply_strings(books_info)
+    books_info, authors_info = get_authors_info(search_strings)
+    reply_strings = get_reply_strings_author(books_info, authors_info)
     reply(comments, reply_strings, table)
     CONN.close()
 
